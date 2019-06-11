a5557f8d78f10767ed597ef9a47ff7a70cb5d200
Niels van der Vegt <ns.vandervegt@gmail.com>
2019-06-12 01:59:42 +0200

    Tutorial 03: Added implementation of exercise where a gene expression heatmap is created using an external R script.

---
+++ 03-calling-scripts-from-the-pipeline/heatmap.R
@@ -0,0 +1,12 @@
+gene_expression <- as.matrix(read.csv(args[1], header=FALSE, sep=",")[-1,-1])
+rownames(gene_expression) <- read.csv(args[1], header=FALSE, sep=",")[-1,1]
+colnames(gene_expression) <- read.csv(args[1], header=FALSE, sep=",")[1,-1]
+
+png(args[2],
+    width = 1500,
+    height = 1500,
+    res = 300,
+    pointsize = 8)
+
+heatmap(gene_expression)
+dev.off()
